a5792c313ad762956a2af9a048450db9df280c7d
Samit Sarkar <samsarka@microsoft.com>
2020-04-16 23:54:06 +0530

    importing

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HelloWorld.Models;
+using System.Fabric;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            var osNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            ViewData["osver"] = osNameAndVersion.ToString();
+            var nodeName = FabricRuntime.GetNodeContext().NodeName.ToString();
+            ViewData["nodeName"] = nodeName.ToString();
+
+            ViewData["timeNow"] = System.DateTime.Now.ToUniversalTime().ToString();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
